4f40cd13479e6ea21404ce1517caedbedf8a6ad6
Hrodberht <hrodberht.stefanescu@gmail.com>
2023-01-23 09:10:02 +0000

    Use Values library

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   };
 
   return (
@@ -22,6 +28,8 @@ function App() {
             type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025S"
+            className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
             submit
